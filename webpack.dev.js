8895d9db895018ad8225e2b37b0c5a8dc8ecf18a
Okan <okan.dalan11@gmail.com>
2022-04-25 11:51:21 +0300

    change name of the output file for github pages

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,6 +10,6 @@ module.exports = merge(common, {
   },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -6,6 +6,6 @@ module.exports = merge (common,{
   mode: 'production',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
 });
