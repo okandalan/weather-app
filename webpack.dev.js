40f88daddc24d573e0ba89e734990ba0fac5b628
Okan <okan.dalan11@gmail.com>
2022-04-25 11:41:07 +0300

    webpack split dev and production config

---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const common = require('./webpack.common');
+const merge = require('webpack-merge');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+      static: './dist',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+});
