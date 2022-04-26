44ba66df92f36af63c1a2d0d2ac511c4a3e059f2
Okan <okan.dalan11@gmail.com>
2022-04-26 06:51:00 +0300

    change of webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,8 +4,4 @@ const {merge} = require('webpack-merge');
 
 module.exports = merge (common,{
   mode: 'production',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
 });
